914c59303b82d454a83207cc54af97424ba1ad4e
mahaltrupti <truptimahal2004@gmail.com>
2025-08-27 13:31:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+  <script>
+    // Change text color on click
+    const textDiv = document.getElementById("text");
+    textDiv.addEventListener("click", () => {
+      textDiv.style.color = textDiv.style.color === "crimson" ? "#2c3e50" : "crimson";
+    });
+  </script>
